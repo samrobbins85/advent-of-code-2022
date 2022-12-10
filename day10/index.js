3e7b455e97305f926ce13036cc326cf032c0f5d0
Sam Robbins <samrobbinsgb@gmail.com>
2022-12-10 12:56:18 +0000

    Generalise

--- day10/index.js
+++ day10/index.js
@@ -1,14 +1,11 @@
 import { fileToArray } from "../common/utils.js";
 
-export function problem1(array) {
+function genericProblem(array, processor, state) {
   let pendingInstruction = null;
   let X = 1;
-  let counter = 0;
   let arrayIndex = 0;
   for (let i = 1; arrayIndex < array.length; i++) {
-    if ((i - 20) % 40 === 0) {
-      counter += X * i;
-    }
+    processor(i, X, state);
     if (array[arrayIndex] === "noop") {
       arrayIndex++;
     } else if (pendingInstruction) {
@@ -19,33 +16,28 @@ export function problem1(array) {
       pendingInstruction = array[arrayIndex];
     }
   }
-  return counter;
+  return state;
 }
+export const problem1 = (array) =>
+  genericProblem(
+    array,
+    (i, X, state) => !((i - 20) % 40) && (state[0] += X * i),
+    [0]
+  )[0];
 
-function problem2(array) {
-  let pendingInstruction = null;
-  let X = 1;
-  let arrayIndex = 0;
-  let screen = Array(6)
-    .fill(null)
-    .map(() => Array(40).fill("."));
-  for (let i = 1; arrayIndex < array.length; i++) {
-    let virtualI = (i - 1) % 40;
-    if (X + 1 === virtualI || X - 1 === virtualI || X === virtualI) {
-      screen[Math.floor(i / 40)][virtualI] = "#";
-    }
-    if (array[arrayIndex] === "noop") {
-      arrayIndex++;
-    } else if (pendingInstruction) {
-      X += parseInt(pendingInstruction.split(" ")[1], 10);
-      pendingInstruction = null;
-      arrayIndex++;
-    } else if (array[arrayIndex] !== "noop") {
-      pendingInstruction = array[arrayIndex];
-    }
-  }
-  return screen;
-}
+export const problem2 = (array) =>
+  genericProblem(
+    array,
+    (i, X, state) => {
+      let virtualI = (i - 1) % 40;
+      X >= virtualI - 1 &&
+        X <= virtualI + 1 &&
+        (state[Math.floor(i / 40)][virtualI] = "#");
+    },
+    Array(6)
+      .fill(null)
+      .map(() => Array(40).fill("."))
+  );
 
 console.log(problem1(fileToArray("day10/input.txt")));
 console.table(problem2(fileToArray("day10/input.txt")));
