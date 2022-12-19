61871e21d2fadadee05aba8e6b0734e00921f9e2
Sam Robbins <samrobbinsgb@gmail.com>
2022-12-19 15:38:35 +0000

    Part 1

--- day16/index.js
+++ day16/index.js
@@ -22,7 +22,7 @@ function problem1(array) {
       }
     });
   });
-  console.log(paths);
+  // Get shortest paths
   input.forEach((i) => {
     input.forEach((j) => {
       input.forEach((k) => {
@@ -33,7 +33,42 @@ function problem1(array) {
       });
     });
   });
-  console.log(paths);
+  let routes = [];
+  function move(node, visited, timeRemaining) {
+    const options = unstuck
+      .map((item) => item.valve)
+      .filter((item) => !visited.includes(item));
+    if (visited.length === unstuck.length + 1) {
+      routes.push(visited);
+    } else {
+      options.forEach((valve) => {
+        if (timeRemaining - paths[node][valve] < 0) {
+          routes.push(visited);
+        } else {
+          move(valve, [...visited, valve], timeRemaining - paths[node][valve]);
+        }
+      });
+    }
+  }
+  move("AA", ["AA"], 30);
+  return routes
+    .map(
+      (route) =>
+        route.reduce(
+          (prev, curr, i, arr) => {
+            if (i === 0) return prev;
+            const minute = prev.minute + paths[arr[i - 1]][curr] + 1;
+            return {
+              pressure:
+                prev.pressure +
+                input.find((i) => i.valve === curr).flow * (30 - minute),
+              minute,
+            };
+          },
+          { pressure: 0, minute: 0 }
+        ).pressure
+    )
+    .sort((a, b) => b - a)[0];
 }
 
-console.log(problem1(fileToArray("day16/exampleInput.txt")));
+console.log(problem1(fileToArray("day16/input.txt")));
